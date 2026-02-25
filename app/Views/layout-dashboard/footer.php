12dc7f60020212b0cd277f39d79bccc7864b5936
Davis Aji <dvisboy@gmail.com>
2026-02-25 08:51:56 +0700

    Tampilan done

--- app/Views/layout-dashboard/footer.php
+++ app/Views/layout-dashboard/footer.php
@@ -1,12 +1,22 @@
-<footer class="py-4 bg-light mt-auto">
+<footer class="py-3 bg-light mt-auto border-top">
     <div class="container-fluid px-4">
-        <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-            <div>
-                <a href="#">Privacy Policy</a>
-                &middot;
-                <a href="#">Terms &amp; Conditions</a>
+        <div class="d-flex flex-column flex-md-row align-items-center justify-content-between small">
+            
+            <!-- Left -->
+            <div class="text-muted mb-2 mb-md-0">
+                &copy; <?= date('Y') ?> E-Tiket. All rights reserved.
             </div>
+
+            <!-- Right -->
+            <div>
+                <a href="#!" class="text-decoration-none text-muted me-3">
+                    Privacy Policy
+                </a>
+                <a href="#!" class="text-decoration-none text-muted">
+                    Terms &amp; Conditions
+                </a>
+            </div>
+
         </div>
     </div>
 </footer>
